7df4e7d91e4e3074bb3a77a6df74d638ff6b81cb
mwbouwkamp <mwbouwkamp@gmail.com>
2019-12-01 14:24:56 +0100

    Day 1: add tests

--- src/test/java/day1/SolverTest.java
+++ src/test/java/day1/SolverTest.java
@@ -1,22 +1,53 @@
 package day1;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolverTest {
 
+    public static final List<Long> EMPTY_LIST = new ArrayList<>();
+
     @Test
-    void calculateTotalFuel() {
+    void calculate_TotalFuel_should_return_default_value_for_empty_list() {
+        assertEquals(-1, new Solver().calculateTotalFuel(EMPTY_LIST));
     }
 
     @Test
-    void calculateTotalFuelIncludingFuelForFuelTransport() {
+    void calculateTotalFuelIncludingFuelForTransport_should_return_default_value_for_empty_list() {
+        assertEquals(-1, new Solver().calculateTotalFuelIncludingFuelForFuelTransport(EMPTY_LIST));
     }
 
-    @Test
-    void calculateFuel() {
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0",
+            "3, 0",
+            "6, 0",
+            "8, 0",
+            "9, 1",
+            "11, 1",
+            "12, 2"
+    })
+    void calculateFuel_should_return_fuel(long input, long expected) {
+        assertEquals(expected, new Solver().calculateFuel(input));
     }
 
-    @Test
-    void getAdditionalFuelRequired() {
+    @ParameterizedTest
+    @CsvSource({
+            "0, 0",
+            "9, 10", //9 -> 1                       --> 1 + 9 = 10
+            "32, 40", //32 -> 8 -> 0                --> 32 + 8 = 40
+            "33, 43", //33 -> 9 -> 1                --> 33 + 9 + 1 = 43
+            "34, 44", //33 -> 9 -> 1                --> 34 + 9 + 1 = 44
+            "105, 148" //105 -> 33 -> 9 -> 1        --> 105 + 33 + 9 + 1 = 148
+    })
+    void getAdditionalFuelRequired(long input, long expected) {
+        assertEquals(expected, new Solver().getAdditionalFuelRequired(input));
     }
+
 }
