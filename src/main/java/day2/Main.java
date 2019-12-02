19a0d8e9305c60f5364daab16e4ca3e0e74b686b
mwbouwkamp <mwbouwkamp@gmail.com>
2019-12-02 20:11:42 +0100

    Day 2: solution

--- src/main/java/day2/Main.java
+++ src/main/java/day2/Main.java
@@ -1,8 +1,52 @@
 package day2;
 
+import utils.FileUtils;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws FileNotFoundException {
+        String input = FileUtils.getStringFromInput("input2.txt").get(0);
+        List<Integer> numbers = getNumbersFromInput(input);
+
+        //Day 1
+        List<Integer> numbersInput = changeVerbNoun(numbers, 12, 2);
+        List<Integer> numbersResult = new Solver().processIntCode(numbersInput);
+        System.out.println("Solution 2.1: " + numbersResult.get(0));
+
+        //Day 2
+        int i = 0;
+        int j = 0;
+        OUTER:
+        for (i = 0; i < 100; i++) {
+            for (j = 0; j < 100; j++) {
+                numbersInput = changeVerbNoun(numbers, i, j);
+                numbersResult = new Solver().processIntCode(numbersInput);
+                if (numbersResult.get(0) == 19690720) {
+                    break OUTER;
+                }
+            }
+        }
+        System.out.println("Solution 2.2: " + (i * 100 + j));
+    }
+
+    private static List<Integer> changeVerbNoun(List<Integer> numbers, int noun, int verb) {
+        List<Integer> numbersInput = new ArrayList<>(numbers);
+        numbersInput.set(1, noun);
+        numbersInput.set(2, verb);
+        return numbersInput;
+    }
+
+    private static List<Integer> getNumbersFromInput(String input) {
+        return Arrays.stream(input.split(","))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/day2/Solver.java
+++ src/main/java/day2/Solver.java
@@ -1,4 +1,38 @@
 package day2;
 
+import java.util.List;
+
 public class Solver {
+
+    public List<Integer> processIntCode(List<Integer> numbers) {
+        int opCode = 0;
+        int pointer = 0;
+        while(true) {
+            opCode = numbers.get(pointer);
+            pointer = movePosition(pointer, numbers, 1);
+            int firstNum = numbers.get(pointer);
+            pointer = movePosition(pointer, numbers, 1);
+            int secondNum = numbers.get(pointer);
+            pointer = movePosition(pointer, numbers, 1);
+            if (opCode == 1) {
+                numbers.set(numbers.get(pointer), numbers.get(firstNum) + numbers.get(secondNum));
+            }
+            else if (opCode == 2) {
+                numbers.set(numbers.get(pointer), numbers.get(firstNum) * numbers.get(secondNum));
+            }
+            else if (opCode == 99) {
+                break;
+            }
+            else {
+                throw new IllegalArgumentException("Not a valid list of numbers");
+            }
+            pointer = movePosition(pointer, numbers, 1);
+        }
+        return numbers;
+    }
+
+    private int movePosition(int pointer, List<Integer> integerList, int steps) {
+        pointer += steps;
+        return pointer  % integerList.size();
+    }
 }
