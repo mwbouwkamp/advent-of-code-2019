e2e4f2cc8a5c99e06ffaf6a4e3d2accb5269119c
mwbouwkamp <mwbouwkamp@gmail.com>
2019-12-04 16:53:27 +0100

    Day4: Solved

---
+++ src/main/java/day4/Solver.java
@@ -0,0 +1,75 @@
+package day4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solver {
+
+    public List<Integer> getValidPasswords(int start, int end) {
+        List<Integer> validPasswords = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            List<Integer> digits = getDigitsFromInt(i);
+            if (doubleIntegers(digits) && integerListAscending(digits)) {
+                validPasswords.add(i);
+            }
+        }
+        return validPasswords;
+    }
+
+    public List<Integer> getValidPasswordsPart2(int start, int end) {
+        return getValidPasswords(start, end).stream()
+                .filter(this::onlyDoubleIntegers)
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> getDigitsFromInt(int number) {
+        return Arrays.stream(Integer.toString(number).split(""))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    public boolean onlyDoubleIntegers(int number) {
+        String numbersString = getDigitsFromInt(number).stream()
+                .map(i -> Integer.toString(i))
+                .reduce((a, b) -> a + b)
+                .orElse("");
+        numbersString = getWithOnlySingleAndDoubleValues(numbersString);
+        List<Integer> reducedNumbers = Arrays.stream(numbersString.split(""))
+                .filter(s -> s.length() > 0)
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+        return doubleIntegers(reducedNumbers);
+    }
+
+    public String getWithOnlySingleAndDoubleValues(String numbersString) {
+        return numbersString
+                .replaceAll("(\\d)\\1\\1\\1\\1\\1", "")
+                .replaceAll("(\\d)\\1\\1\\1\\1", "")
+                .replaceAll("(\\d)\\1\\1\\1", "")
+                .replaceAll("(\\d)\\1\\1", "");
+    }
+
+    public boolean doubleIntegers(List<Integer> numbers) {
+        for (int i = 1; i < numbers.size(); i++) {
+            if (numbers.get(i) == numbers.get(i - 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean integerListAscending(List<Integer> numbers) {
+        int curInt = numbers.get(0);
+        for (int i = 1; i< numbers.size(); i++) {
+            if (numbers.get(i) < numbers.get(i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
