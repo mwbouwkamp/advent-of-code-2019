2737993dda4a8ded9116fdf8bf623ad687a23046
mwbouwkamp <mwbouwkamp@gmail.com>
2019-12-04 18:34:20 +0100

    Day 4: Added test for integerListAscending

--- src/test/java/day4/SolverDay4Test.java
+++ src/test/java/day4/SolverDay4Test.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolverDay4Test {
@@ -19,5 +24,24 @@ class SolverDay4Test {
     })
     void removeTrippleAndHigher(String input, String expected) {
         assertEquals(expected, new SolverDay4().removeTrippleAndHigher(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'1',true",
+            "'1,2',true",
+            "'2,1',false",
+            "'1,2,3,4,5',true",
+            "'5,4,3,2,1',false",
+            "'1,2,3,2,3',false",
+            "'1,1',true"
+    })
+    void integerListAscending(String input, boolean expected) {
+        List<Integer> intList = Arrays.stream(input.split(","))
+                .mapToInt(Integer::parseInt)
+                .boxed()
+                .collect(Collectors.toList());
+        boolean result = new SolverDay4().integerListAscending(intList);
+        assertEquals(expected, result);
     }
 }
